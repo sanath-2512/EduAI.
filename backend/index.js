785eb2a976461fd34cdca17fe50a978cb405cb17
sanath-2512 <sanathwaraikar@gmail.com>
2025-11-13 18:22:24 +0530

    deployed error resolve

--- backend/index.js
+++ backend/index.js
@@ -8,10 +8,7 @@ const { PrismaClient } = require('@prisma/client');
 const app = express();
 const prisma = new PrismaClient();
 
-app.use(cors({
-    origin: "https://edu-ai-rho-hazel.vercel.app/", // allow Vite frontend
-    credentials: true,
-  }));
+app.use(cors());
   
 
 
